e62a5b48b8950b6dd5bb30c1acb621454593def7
shine.Rain <shinerainy_ycy@163.com>
2022-07-07 16:24:44 +0800

    SerialGC 实际测试内存分配策略

--- src/org/fenixsoft/jvm/chapter3/TestSerialGCAllocation.java
+++ src/org/fenixsoft/jvm/chapter3/TestSerialGCAllocation.java
@@ -1,15 +1,16 @@
 package org.fenixsoft.jvm.chapter3;
 
 /**
- * VM参数：+XX:UseSerialGC
- * 
+ * VM参数：-XX:+UseSerialGC
  */
 public class TestSerialGCAllocation {
 
     private static final int _1MB = 1024 * 1024;
 
     /**
+     * 触发Minor GC
      * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+     * -verbose:gc  等同于 -XX:+PrintGC 等同于 -Xlog:gc
      */
     public static void testAllocation() {
         byte[] allocation1, allocation2, allocation3, allocation4;
@@ -20,8 +21,9 @@ public class TestSerialGCAllocation {
     }
 
     /**
+     * 大对象直接进入老年代
      * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
-     * -XX:PretenureSizeThreshold=3145728
+     * -XX:PretenureSizeThreshold=3145728 大对象内存限制，超过该内存直接进入老年代 只支持Serial 和 Parnew
      */
     public static void testPretenureSizeThreshold() {
         byte[] allocation;
@@ -29,6 +31,7 @@ public class TestSerialGCAllocation {
     }
 
     /**
+     * 长期存活的对象进入老年代
      * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1
      * -XX:+PrintTenuringDistribution
      */
@@ -42,6 +45,7 @@ public class TestSerialGCAllocation {
     }
 
     /**
+     * 动态对象年龄判定 Survivor年龄小于等于某年龄的所有对象大小和占据过半则年龄大者入老年代
      * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15
      * -XX:+PrintTenuringDistribution
      */
@@ -56,8 +60,29 @@ public class TestSerialGCAllocation {
     }
 
     /**
-     * VM参数：-Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:-HandlePromotionFailure
+     * 均采用SerialGC 非region布局好测试
+     * 1.8默认Paraller Scaveng + Paraller Old
+     * 11+ 默认 G1
      */
+    public static void main(String[] args) {
+        // 触发Minor GC
+//        TestSerialGCAllocation.testAllocation();
+        // 大对象直接进入老年代
+//        TestSerialGCAllocation.testPretenureSizeThreshold();
+        // 长期存活的对象进入老年代
+//        TestSerialGCAllocation.testTenuringThreshold();
+        // 动态对象年龄判定 Survivor年龄小于等于某年龄的所有对象大小和占据过半则年龄大者入老年代
+//        TestSerialGCAllocation.testTenuringThreshold2();
+        // 空间分配担保 - 开关已过期 JDK Update 24规则已更改 老年代剩余空间 小于新生代对象总大小 或者 小于历次晋升总大小，则直接Full gc
+//        TestSerialGCAllocation.testHandlePromotion();
+    }
+
+    /**
+     * 空间分配担保
+     * VM参数：-Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:-HandlePromotionFailure
+     * -XX:-HandlePromotionFailure 老年代剩余空间小于历次Minor GC晋升的老年代对象时，是否冒险继续进行Minor GC
+     */
+    @Deprecated
     public static void testHandlePromotion() {
         byte[] allocation1, allocation2, allocation3, allocation4, allocation5, allocation6, allocation7;
         allocation1 = new byte[2 * _1MB];
@@ -72,7 +97,5 @@ public class TestSerialGCAllocation {
         allocation6 = null;
         allocation7 = new byte[2 * _1MB];
     }
-
-
 
 }
