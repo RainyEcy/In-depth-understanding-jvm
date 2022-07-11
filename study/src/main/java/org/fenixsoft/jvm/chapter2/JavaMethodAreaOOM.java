8b0888077f6dd73a398a7fd563e5b318b0c97e98
shine.Rain <shinerainy_ycy@163.com>
2022-07-11 21:16:17 +0800

    Maven布局

--- study/src/main/java/org/fenixsoft/jvm/chapter2/JavaMethodAreaOOM.java
+++ study/src/main/java/org/fenixsoft/jvm/chapter2/JavaMethodAreaOOM.java
@@ -1,34 +1,34 @@
-package org.fenixsoft.jvm.chapter2;
-
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
-
-import java.lang.reflect.Method;
-
-/**
- * 方法区储存类型信息，动态代理无限生成新类型使方法区内存溢出
- * VM Args： -XX:PermSize=10M -XX:MaxPermSize=10M
- * -XX:PermSize=10M 方法区初始化内存
- * -XX:MaxPermSize=10M 方法区最大内存
- */
-public class JavaMethodAreaOOM {
-
-    public static void main(String[] args) {
-        while (true) {
-            Enhancer enhancer = new Enhancer();
-            enhancer.setSuperclass(OOMObject.class);
-            enhancer.setUseCache(false);
-            enhancer.setCallback(new MethodInterceptor() {
-                @Override
-                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
-                    return proxy.invokeSuper(obj, args);
-                }
-            });
-            enhancer.create();
-        }
-    }
-
-    static class OOMObject {
-    }
-}
+//package org.fenixsoft.jvm.chapter2;
+//
+//import net.sf.cglib.proxy.Enhancer;
+//import net.sf.cglib.proxy.MethodInterceptor;
+//import net.sf.cglib.proxy.MethodProxy;
+//
+//import java.lang.reflect.Method;
+//
+///**
+// * 方法区储存类型信息，动态代理无限生成新类型使方法区内存溢出
+// * VM Args： -XX:PermSize=10M -XX:MaxPermSize=10M
+// * -XX:PermSize=10M 方法区初始化内存
+// * -XX:MaxPermSize=10M 方法区最大内存
+// */
+//public class JavaMethodAreaOOM {
+//
+//    public static void main(String[] args) {
+//        while (true) {
+//            Enhancer enhancer = new Enhancer();
+//            enhancer.setSuperclass(OOMObject.class);
+//            enhancer.setUseCache(false);
+//            enhancer.setCallback(new MethodInterceptor() {
+//                @Override
+//                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+//                    return proxy.invokeSuper(obj, args);
+//                }
+//            });
+//            enhancer.create();
+//        }
+//    }
+//
+//    static class OOMObject {
+//    }
+//}
--- study/src/main/java/org/fenixsoft/jvm/chapter4/BTraceTest.java
+++ study/src/main/java/org/fenixsoft/jvm/chapter4/BTraceTest.java
@@ -26,23 +26,22 @@ public class BTraceTest {
 }
 
 /* BTrace Script Template
-import com.sun.btrace.annotations.*;
-        import static com.sun.btrace.BTraceUtils.*;
+    import com.sun.btrace.annotations.*;
+            import static com.sun.btrace.BTraceUtils.*;
 
-@BTrace
-public class TracingScript {
-    @OnMethod(
-            clazz="org.fenixsoft.monitoring.BTraceTest",
+    @BTrace
+    public class TracingScript {
+       @OnMethod(
+            clazz="org.fenixsoft.jvm.chapter4.BTraceTest",
             method="add",
             location=@Location(Kind.RETURN)
     )
-
-    public static void func(@Self org.fenixsoft.monitoring.BTraceTest instance,int a,int b,@Return int result) {
+    public static void func(@Self Object instance,int a,int b,@Return int result) {
         println("调用堆栈:");
         jstack();
         println(strcat("方法参数A:",str(a)));
         println(strcat("方法参数B:",str(b)));
         println(strcat("方法结果:",str(result)));
     }
-}
+    }
 */
